76fba5414ce15681bccbd31020fc19fa1208caa2
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-24 08:36:04 -0800

    Update rpc.rb

--- lib/floss/rpc.rb
+++ lib/floss/rpc.rb
@@ -1,22 +1,24 @@
-require 'floss'
-
-module Floss::RPC
-  TIMEOUT = 0.3
-
-  class Client
-    def call(command, payload)
-      raise NotImplementedError
-    end
-  end
-
-  # Listens to a ZMQ Socket and handles commands from peers.
-  class Server
-    attr_accessor :address
-    attr_accessor :handler
-
-    def initialize(address, &handler)
-      self.address = address
-      self.handler = handler
-    end
-  end
-end
+require 'floss' module 
+  Floss::RPC   TIMEOUT
+  = 0.3
+  class 
+    Client  def call
+      (command, payload) raise 
+      Not
+      Implemented
+      Error    end 
+          end
+  # Listens to a
+  ZMQ Socket
+  and handles commands 
+  from peers.   class
+  Server   attr_accessor
+  :address   attr_accessor
+  :handler
+    def
+  initialize
+      (address, &  handler) self.address =
+      address self.handler =
+        handler  end 
+        end  
+              end
