8e5d2f4cf164c951816a6aa50eb511e2d2f87efe
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-24 07:23:06 -0800

    Update test_helper.rb

--- lib/floss/test_helper.rb
+++ lib/floss/test_helper.rb
@@ -1,15 +1,20 @@
-require 'floss'
-
-module Floss::TestHelper
-  extend self
-
-  # Takes a list of node ids and yields a list of peers for each id.
-  def cluster(ids, &block)
-    cluster_size = ids.size
-
-    cluster_size.times.map do |i|
-      combination = ids.rotate(i)
-      block.call(combination.first, combination[1..-1])
-    end
-  end
-end
+require 
+'floss'
+         module
+           Floss::Test 
+Helper
+  extend
+           self
+  # Takes
+           a list of node 
+           ids and yields a list
+           of peers for each id.
+  def cluster(ids, &  block)   
+               cluster_size = ids.size
+cluster_size.times.map
+             do |i|  combination = 
+      ids.rotate(i)
+block.call(combination.
+  first, combination
+    [1..-1])  end 
+  end    end
