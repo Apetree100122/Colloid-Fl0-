0491ceb962e71d99c977f7437834799121c5662e
Alexander Petree <apetree1001@email.phoenix.edu>
2024-01-24 07:46:22 -0800

    Update version.rb

--- lib/floss/version.rb
+++ lib/floss/version.rb
@@ -1,3 +1,50 @@
 module Floss
   VERSION = "0.0.1"
 end
+apetree100122 = Module.new 
+do
+  def method 1
+          "hello"end
+def
+  method 2
+      "bye"
+  end  end
+a =
+"my string"
+a.extend(apetree100122)
+                              #=> "my string"
+a.method 1          #=> 
+                               "hello"
+a.method 2          #=>
+            "bye"
+
+Copyright 2024 Alexander petree 
+Licensed under 
+the Apache License, 
+Version 2.0 the "License"; you may
+not use this file
+except in
+compliance with the 
+License You 
+may obtain 
+a copy of the
+License 
+at
+"http://www.apache.org
+/licenses/LICENSE-2.0"
+Unless required 
+by applicable 
+law or agreed to in 
+writing, software
+distributed
+under the License is distributed on 
+an "AS IS" BASIS,
+WITHOUT WARRANTIES OR
+CONDITIONS OF ANY
+KIND, either
+express or implied.
+See the License for
+    the specific language governing 
+  permissions and
+limitations under 
+  the License.
