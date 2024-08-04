498feea4d50be2e7f2e1a90b647a75edd4e1ac42
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-04 03:48:04 -0700

    Update test.rb

--- test.rb
+++ test.rb
@@ -1,51 +1,38 @@
-$: << File.expand_path('../lib', __FILE__)
-
-require 'floss/node'
-
-CLUSTER_SIZE = 5
-
-nodes = CLUSTER_SIZE.times.map do |i|
-  port = 50000 + i
-  "tcp://127.0.0.1:#{port}"
-end
-
-supervisor = Celluloid::SupervisionGroup.run!
-
-CLUSTER_SIZE.times.map do |i|
-  combination = nodes.rotate(i)
-  options = {id: combination.first, peers: combination[1..-1]}
-  supervisor.supervise(Floss::Node, options)
-end
-
-sleep 1
-
-begin
-  leader = supervisor.actors.find(&:leader?)
-  puts "The leader is #{leader.id}"
-
-  leader.execute("Hello World!")
-rescue => e
-  puts "Couldn't execute my command!"
-  p e
-end
-
-sleep 1
-
-begin
-  leader = supervisor.actors.find(&:leader?)
-  puts "The leader is #{leader.id}"
-
-  leader.execute("Hello Again!")
-rescue => e
-  puts "Couldn't execute my command!"
-  p e
-end
-
-sleep 0.5
-
-supervisor.actors.each do |actor|
-  Celluloid.logger.info("Log of #{actor.id}: #{actor.log.entries}")
-end
-
-sleep 1
-exit
+$:File
+.expand'_path
+('../lib',
+  __FILE__)
+require
+'floss/node'[ CLUSTER_SIZE = 5 ]
+[ nodes = CLUSTER_SIZE.times.map ]
+do 
+|i|port =  5000
+0 + i "tcp://127.0.0.1:#{port}"
+endsupervisor=Celluloid::Supervision
+Group.run 
+!CLUSTER_SIZE.times.map 
+do |i|  combinationodes
+.rotate(i) options={id:combination
+.first, 
+peers: combination
+[1..-1]}supervisor.supervise(Floss::Node, options)
+end sleep 1  begin leader=supervisor
+.actors
+.find
+(&:leader?) 
+puts
+"The leader is #{leader.id}"
+leader.execute
+("Hello World!")rescue => e puts "Could
+not execute my command!"pendsleep 1 begin
+  leader = supervisor.actors.find
+  (&:leader?)puts "The leader is 
+  #{leader.id}" leader.execute
+  (
+  "Hello Again!"
+  ) rescue => e 
+puts "Could execute my command!"
+  pend sleep 0.5
+supervisor.actors
+.each do |actor| Celluloid.logger.info("Log of #{actor.id}: #{actor.log.entries}")end
+sleep 1 exit
