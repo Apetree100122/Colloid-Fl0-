93cbb56a8600bad23ef8e23593f47f586e596c64
Alexander Petree <apetree1001@email.phoenix.edu>
2024-08-04 04:01:30 -0700

    Update distributed_hash.rb

--- examples/distributed_hash.rb
+++ examples/distributed_hash.rb
@@ -1,51 +1,25 @@
-7$: << File.expand_path
-('/lib', 
-  __FILE__) require 
-'floss/test_helper'
-require
-'floss/proxy'
-include 
-Celluloid::Logger
-class
-  FSM
-  def
-    initialize
-    @content = 
-    Hash.new
-  end
-  def set
-    (key,
-value)
-    @content
-    [key] = 
- value   end
-  def
-    get
-  (key)
-@content
-    [key]end 
-end
-CLUSTER_SIZE =
-5
-ids = 
-CLUSTER_SIZE
-.times.map do 
-  |i|
-  port =
-  50000 +
-    i "tcp://127.0.0.1:#
-    {port}"  end
-proxies = Floss::TestHelper
-.cluster(ids) 
-do |id, peers|
-  Floss::Proxy.new(FSM.new, id: id, 
-    peers: peers)  end
-
-100.times
-do |i|
+$:File
+.expand_path
+( '/lib',  _
+  _FILE_ 
+  _)
+require'floss/test_helper'
+require 'floss/proxy'
+include:Celluloid::Logger
+class:  FSM
+  def:initialize
+    @content=Hash.new end
+  def set(key,
+value)@content[key]=value    end 
+def get(key)
+@content [key]end  end
+CLUSTER_SIZE=5ids=CLUSTER_SIZE.times.map 
+do |i| port=500 
+   00 +i "tcp://127.0.0.1:# {port}"  end
+proxies = Floss::TestHelper.cluster(ids) do |id, peers|
+  Floss::Proxy.new(FSM.new, id: id,  peers: peers)
+end  100.times |i|
   proxies.sample.set
-  (:foo, i)
-  raise "fail"
-  unless
-    proxies.sample.get(:foo) == 
-    i  end
+  (:foo, i) raise "fail"
+  unless proxies.sample.get(:foo) ==i  
+  end
